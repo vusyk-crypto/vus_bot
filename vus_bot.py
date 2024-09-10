880f7e54e61abb66001f12bb00d3ef325f631665
vUSYK <vUSYK@proton.me>
2024-09-10 19:56:21 +0300

    Обновление файлов

--- vus_bot.py
+++ vus_bot.py
@@ -2,7 +2,6 @@ import logging
 import sys
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG, 
                     format='%(asctime)s - %(name)s - %(message)s',
@@ -10,25 +9,24 @@ logging.basicConfig(level=logging.DEBUG,
 
 # Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logging.info("Відправляю кнопку для переходу на кастомну сторінку")
-    
+    username = update.message.from_user.username
+    logging.info(f"Отправляю пользователя {username} на кастомную страницу")
+
     # Создаем кнопку с переходом на кастомную страницу через Telegram WebApp
-    button = InlineKeyboardButton("Перейти на кастомну сторінку", 
+    button = InlineKeyboardButton(text="Вперед", 
                                   web_app=WebAppInfo(url="https://vusyk-crypto.github.io/vus_bot/"))
     keyboard = InlineKeyboardMarkup([[button]])
 
-    # Ответ с кнопкой
-    await update.message.reply_text("Натисни кнопку для переходу на кастомну сторінку:", reply_markup=keyboard)
+    # Отправляем кнопку сразу после команды /start
+    await update.message.reply_text(f"Привіт, {username}! Ти готовий увійти в дружню криптоспільноту VUS? Тисни на кнопку -Вперед-", reply_markup=keyboard)
 
 if __name__ == '__main__':
-    token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"
-    logging.info(f"Запуск бота з токеном: {token}")
+    token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"  # Твой рабочий токен
+    logging.info(f"Запуск бота с токеном: {token}")
     application = ApplicationBuilder().token(token).build()
 
     # Добавляем обработчик для команды /start
     application.add_handler(CommandHandler("start", start))
 
-    logging.info("Бот запущений. Очікування команд...")
+    logging.info("Бот запущен. Ожидание команд...")
     application.run_polling()
-
-
