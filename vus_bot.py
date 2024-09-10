d4c8b6395b3519c33b7c0402b1dc2655c486cda8
vUSYK <vUSYK@proton.me>
2024-09-10 19:06:54 +0300

    Удаление старых файлов и обновление проекта vus_bot

--- vus_bot.py
+++ vus_bot.py
@@ -1,39 +1,34 @@
 import logging
 import sys
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG, 
-                    format='%(asctime)s - %(name)s - %(name)s - %(message)s',
+                    format='%(asctime)s - %(name)s - %(message)s',
                     stream=sys.stdout)
 
 # Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    response = f"Привіт! Це нова відповідь. Поточний час: {current_time}"
-    logging.info(f"Відправляю відповідь: {response}")
-    await update.message.reply_text(response)
-
-# Обработчик команды /main с кнопкой для перехода на кастомную страницу
-async def main(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Создаем кнопку с ссылкой на кастомную страницу
-    button = InlineKeyboardButton("Перейти на кастомну сторінку", url="http://localhost:8000/web/custom_page.html")
+    logging.info("Відправляю кнопку для переходу на кастомну сторінку")
+    
+    # Создаем кнопку с переходом на кастомную страницу через Telegram WebApp
+    button = InlineKeyboardButton("Перейти на кастомну сторінку", 
+                                  web_app=WebAppInfo(url="https://vusyk-crypto.github.io/vus_bot/"))
     keyboard = InlineKeyboardMarkup([[button]])
 
-    response = "Це відповідь на команду /main."
-    logging.info(f"Відправляю відповідь: {response}")
-    await update.message.reply_text(response, reply_markup=keyboard)
+    # Ответ с кнопкой
+    await update.message.reply_text("Натисни кнопку для переходу на кастомну сторінку:", reply_markup=keyboard)
 
 if __name__ == '__main__':
     token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"
     logging.info(f"Запуск бота з токеном: {token}")
     application = ApplicationBuilder().token(token).build()
 
-    # Добавляем обработчики для команд
+    # Добавляем обработчик для команды /start
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("main", main))
 
     logging.info("Бот запущений. Очікування команд...")
     application.run_polling()
+
 
