5f79c0802a3f34dff45c6d78ef7b6b91d9a9f9f7
vUSYK <vUSYK@proton.me>
2024-09-17 12:29:37 +0300

    Обновление кастомной страницы: Делаем полноэкранку при запуске бота

--- vus_bot.py
+++ vus_bot.py
@@ -1,33 +1,35 @@
 import logging
 import sys
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-logging.basicConfig(level=logging.DEBUG, 
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(message)s',
                     stream=sys.stdout)
 
-# Обработчик команды /start
+# Обробник команди /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.message.from_user.username
-    logging.info(f"Отправляю пользователя {username} на кастомную страницу")
+    logging.info(f"Відправляю користувача {username} на кастомну сторінку")
 
-    # Создаем кнопку с переходом на кастомную страницу через Telegram WebApp
+    # Створюємо кнопку з переходом на кастомну сторінку через Telegram WebApp
     web_app_url = f"https://vusyk-crypto.github.io/vus_bot/?username={username}"
-    button = InlineKeyboardButton(text="Вперед", 
-                                  web_app=WebAppInfo(url=web_app_url))
-    keyboard = InlineKeyboardMarkup([[button]])
+    button = KeyboardButton(text="Вперед", web_app=WebAppInfo(url=web_app_url))
+    keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True, one_time_keyboard=True)
 
-    # Отправляем кнопку сразу после команды /start
-    await update.message.reply_text(f"Привіт, {username}! Ти готовий увійти в дружню криптоспільноту VUS? Тисни на кнопку -Вперед-", reply_markup=keyboard)
+    # Відправляємо кнопку одразу після команди /start
+    await update.message.reply_text(
+        f"Привіт, {username}! Ти готовий увійти в дружню криптоспільноту VUS? Тисни на кнопку <<Вперед>>",
+        reply_markup=keyboard
+    )
 
 if __name__ == '__main__':
-    token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"  # Твой рабочий токен
-    logging.info(f"Запуск бота с токеном: {token}")
+    token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"  # Замініть на ваш реальний токен
+    logging.info(f"Запуск бота з токеном: {token}")
     application = ApplicationBuilder().token(token).build()
 
-    # Добавляем обработчик для команды /start
+    # Додаємо обробник для команди /start
     application.add_handler(CommandHandler("start", start))
 
-    logging.info("Бот запущен. Ожидание команд...")
+    logging.info("Бот запущений. Очікування команд...")
     application.run_polling()
