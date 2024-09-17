1e4173164c80fed52d0c86a4f23aee3d77bdd003
vUSYK <vUSYK@proton.me>
2024-09-17 15:12:06 +0300

    Обновление кастомной страницы: about

--- vus_bot.py
+++ vus_bot.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InputFile
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(level=logging.DEBUG,
@@ -14,22 +14,30 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     # Створюємо кнопку з переходом на кастомну сторінку через Telegram WebApp
     web_app_url = f"https://vusyk-crypto.github.io/vus_bot/?username={username}"
-    button = KeyboardButton(text="Вперед", web_app=WebAppInfo(url=web_app_url))
+    
+    # Додаємо кнопку "Вперед"
+    button = KeyboardButton(text="🚀 Вперед", web_app=WebAppInfo(url=web_app_url))
     keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True, one_time_keyboard=True)
 
     # Форматуємо ім'я користувача та VUS жирним шрифтом
     formatted_message = (
         f"Привіт, *{username}*! "
         f"Ти готовий увійти в дружню криптоспільноту *VUS*? "
-        f"Тисни на кнопку '🚀 Вперед'"
+        f"Тисни на кнопку '🚀 Вперед!'"
     )
 
-    # Відправляємо кнопку одразу після команди /start
+    # Відправляємо повідомлення з кнопкою
     await update.message.reply_text(
         formatted_message,
         reply_markup=keyboard,
         parse_mode="Markdown"  # Використовуємо Markdown для форматування
     )
+
+# Обробник для відправки фото
+async def send_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Відправляємо зображення
+    photo_path = 'vus.png'  # Вкажи шлях до своєї картинки
+    await context.bot.send_photo(chat_id=update.message.chat_id, photo=InputFile(photo_path))
 
 if __name__ == '__main__':
     token = "7230512720:AAG0mtv0oUBi9HAh-kCUex9YF-JNmkQXSxQ"  # Замініть на ваш реальний токен
@@ -38,6 +46,9 @@ if __name__ == '__main__':
 
     # Додаємо обробник для команди /start
     application.add_handler(CommandHandler("start", start))
+
+    # Додаємо обробник для відправки зображення
+    application.add_handler(CommandHandler("send_image", send_image))
 
     logging.info("Бот запущений. Очікування команд...")
     application.run_polling()
